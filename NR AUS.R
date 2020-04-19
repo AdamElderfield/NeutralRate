2f0e34f64ac125137b7f20a2e280c939e143bd52
Adam Elderfield <aelderfield@outlook.com>
2020-04-19 17:27:32 +1000

    Using LW three step approach
    Model is giving very plausable estimates. The output gap is positve at the end of estimation, which suggests something off with the Phillips curve.

--- NR AUS.R
+++ NR AUS.R
@@ -184,7 +184,7 @@ buildNRDLM <- function(p){
    
    R[9,9] <- p[11]
    R[11,11] <- p[12]
-   R[12,12] <- p[13]+p[8]
+   R[12,12] <- p[13]+p[8]*p[4]
    R[12,2] <- p[4]*p[8]
    R[14,14] <- p[14]+p[15]
    R[17,14] <- p[6]*p[15]
@@ -282,16 +282,490 @@ cbind(filtered$y[-c(2:9),3],smoothed$s[-c(1:9),7]) %>% matplot(type ="l")
 
 
 #--------------------------------------------------------------------------------------------------------------------------
-# Gibbs sampling
+# Using the LW three stage approach
 #--------------------------------------------------------------------------------------------------------------------------
 
+  
+  stage1NRDLM <- dlm(
+    
+    FF = matrix(0,4,12),
+    
+    V = diag(0.00001, 4),
+    
+    GG =diag(0,12),
+    
+    W = diag(0,12),
+    
+    m0 = rep(0,12),
+    
+    C0 = diag(10000000,12)
+    
+    
+  )
+  
+  # Matrix to parametrise VCV matrix W
+  R <- diag(0,12)
+  
 
-MCMC <- 1500 # number of MC draws
+  # build DLM
+  stage1buildNRDLM <- function(p){
+    
+    FF(stage1NRDLM)[1,1] <- 1
+    FF(stage1NRDLM)[1,2] <- 1
+    
+    FF(stage1NRDLM)[2,6] <- 1
+    FF(stage1NRDLM)[2,7] <- 1
+    
+    FF(stage1NRDLM)[3,9] <- 1
+    
+    FF(stage1NRDLM)[4,12] <- 1
+
+    GG(stage1NRDLM)[1,1]  <- 1 
+    GG(stage1NRDLM)[1,5]  <- 1
+    GG(stage1NRDLM)[2,2]  <- p[1] 
+    GG(stage1NRDLM)[2,3]  <- p[2]
+    GG(stage1NRDLM)[3,2]  <- 1
+    GG(stage1NRDLM)[4,3]  <- 1
+  
+    GG(stage1NRDLM)[5,5] <- 1
+    GG(stage1NRDLM)[6,6] <- 1
+    
+    GG(stage1NRDLM)[7,2]  <- p[3]*(0.4*p[1]+0.3)        
+    GG(stage1NRDLM)[7,3]  <- p[3]*(0.4*p[2]+0.2)        
+    GG(stage1NRDLM)[7,4]  <- p[3]*0.1       
+    GG(stage1NRDLM)[8,7] <- 1           
+    
+    GG(stage1NRDLM)[9,8] <- p[4]
+    GG(stage1NRDLM)[9,9] <- p[5]/3
+    GG(stage1NRDLM)[9,10] <- p[5]/3
+    GG(stage1NRDLM)[9,11] <- p[5]/3
+    GG(stage1NRDLM)[9,12] <- (1-p[5])
+    GG(stage1NRDLM)[10,9] <- 1
+    GG(stage1NRDLM)[11,10] <- 1
+    
+    GG(stage1NRDLM)[12,12] <- 1
+    
+    
+    # Variance covariance - RR'
+    
+    R[1,1] <- p[6]
+    R[2,2] <- p[7]
+    R[6,6] <- p[8]
+    R[7,7] <- p[9]+p[4]*p[7]
+    R[7,2] <- p[4]*p[7]
+    R[9,9] <- p[10]+(1-p[5])*p[11]
+    R[12,9] <- (1-p[5])*p[11]
+    R[12,12] <- p[11]
+    
+    W(stage1NRDLM) <- R%*%t(R)
+
+    # PRIORS
+    m0(stage1NRDLM) <- c(NRdata$log.output[1],0,0,0,mean(diff(NRdata$log.output[1:4])),NRdata$unr[1],0,0,NRdata$Inflation[3],NRdata$Inflation[2],NRdata$Inflation[1], NRdata$Inflation.e[1])
+    
+    
+
+    return(stage1NRDLM)
+    
+  }
+  
+  theta <- c(1.53,-0.54, -0.15, -0.62, 0.32, sqrt(0.38), sqrt(0.54), sqrt(0.05), sqrt(0.15), sqrt(0.07), sqrt(0.79),0.5) # estimates from RBA  paper
+  
+
+  LC <- rep(-Inf,15)
+  UC <- rep(Inf,15)
+  # UC[3] <- -0.0025
+  # LC[c(7:9,11:15)] <- exp(-8)
+  # UC[c(7:9,11:15)] <- exp(12)
+  
+  
+  stage1NRDLM.est <-  dlmMLE(y = cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation, NRdata$Inflation.e), parm = theta, build = stage1buildNRDLM, 
+                       lower = LC, upper = UC,
+                       control = list(trace = 6, REPORT = 5, maxit = 1000), debug = FALSE, method = "L-BFGS-B", hessian = TRUE)
+  
+  
+#--------------------------------------------------------------------------------------------------------------------------
+# filtered and smoothed estimates
+#--------------------------------------------------------------------------------------------------------------------------
+
+stage1NRDLMbuilt <- stage1buildNRDLM(stage1NRDLM.est$par)
 
 
-theta <- c(1.53,-0.54, -0.05, -0.62, -0.32, 0.39, sqrt(0.38), sqrt(0.54), sqrt(0.05), 4, 1.113391 , sqrt(0.15), sqrt(0.07), sqrt(0.79),0.5)  #starting values
+stage1filtered <- dlmFilter(y =cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation, NRdata$Inflation.e), mod = stage1NRDLMbuilt)
+
+stage1smoothed <- dlmSmooth(y = cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation,NRdata$Inflation.e), mod = stage1NRDLMbuilt)
 
 
+cbind(filtered$y[-c(2:9),1],filtered$m[-c(1:9),1]) %>% matplot(type ="l")
+
+cbind(filtered$y[-c(2:9),1],smoothed$s[-c(1:9),1]) %>% matplot(type ="l")
+
+cbind(stage1filtered$m[-c(2:9),5],stage1smoothed$s[-c(1:9),5]) %>% matplot(type ="l")
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+# Median unbiased estimate of the variance for mu (ypot growth) 
+#--------------------------------------------------------------------------------------------------------------------------
+
+# Using potential output (smoothed)
+# function does a rolling regression (chow break point test) on smoothed potential and a constant, searching for a break and returns lambda g (see paper for more details)
+lambda.g <- median.unbiased.estimator.stage1(stage1smoothed$s[,1])
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+# Using the LW three stage approach - stage 2
+#--------------------------------------------------------------------------------------------------------------------------
+
+# mu becomes time varying, we introduce inerest rates and a constant
+
+
+stage2NRDLM <- dlm(
+  
+  FF = matrix(0,5,15),
+  
+  V = diag(0.00001, 5),
+  
+  GG =diag(0,15),
+  
+  W = diag(0,15),
+  
+  m0 = rep(0,15),
+  
+  C0 = diag(10000000,15)
+  
+  
+)
+
+# Matrix to parametrise VCV matrix W
+R <- diag(0,15)
+
+
+# build DLM
+stage2buildNRDLM <- function(p){
+  
+  FF(stage2NRDLM)[1,1] <- 1
+  FF(stage2NRDLM)[1,2] <- 1
+  
+  FF(stage2NRDLM)[2,9] <- 1
+  FF(stage2NRDLM)[2,10] <- 1
+  
+  FF(stage2NRDLM)[3,12] <- 1
+  
+  FF(stage2NRDLM)[4,15] <- 1
+
+  FF(stage2NRDLM)[5,6] <- 1
+  
+  
+  GG(stage2NRDLM)[1,1]  <- 1 
+  GG(stage2NRDLM)[1,5]  <- 1
+  GG(stage2NRDLM)[2,2]  <- p[1] 
+  GG(stage2NRDLM)[2,3]  <- p[2]
+  GG(stage2NRDLM)[2,6]  <- p[3]/2
+  GG(stage2NRDLM)[2,7]  <- p[3]/2
+  GG(stage2NRDLM)[2,8]  <- 1
+  GG(stage2NRDLM)[2,5]  <- p[14]
+  GG(stage2NRDLM)[3,2]  <- 1
+  GG(stage2NRDLM)[4,3]  <- 1
+  
+  GG(stage2NRDLM)[5,5] <- 1
+  GG(stage2NRDLM)[6,6] <- 1
+  GG(stage2NRDLM)[7,6] <- 1
+  GG(stage2NRDLM)[8,8] <- 1
+  
+  GG(stage2NRDLM)[9,9] <- 1
+  
+  GG(stage2NRDLM)[10,2]  <- p[4]*(0.4*p[1]+0.3)        
+  GG(stage2NRDLM)[10,3]  <- p[4]*(0.4*p[2]+0.2)        
+  GG(stage2NRDLM)[10,4]  <- p[4]*0.1       
+  GG(stage2NRDLM)[10,6]  <- p[4]*0.4*p[3]/2       
+  GG(stage2NRDLM)[10,7]  <- p[4]*0.4*p[3]/2       
+  GG(stage2NRDLM)[10,8] <- p[4]*0.4            
+  GG(stage2NRDLM)[10,5] <- p[4]*0.4*p[14]            
+  
+  GG(stage2NRDLM)[11,10] <- 1           
+  
+  GG(stage2NRDLM)[12,11] <- p[5]
+  GG(stage2NRDLM)[12,12] <- p[6]/3
+  GG(stage2NRDLM)[12,13] <- p[6]/3
+  GG(stage2NRDLM)[12,14] <- p[6]/3
+  GG(stage2NRDLM)[12,15] <- (1-p[6])
+  GG(stage2NRDLM)[13,12] <- 1
+  GG(stage2NRDLM)[14,13] <- 1
+
+  GG(stage2NRDLM)[15,15] <- 1
+  
+  
+  # Variance covariance - RR'
+  
+  R[1,1] <- p[7]
+  R[2,2] <- p[8]
+  R[5,5] <-lambda.g*p[7]
+  R[9,9] <- p[9]
+  R[10,10] <- p[10]+p[4]*p[8]
+  R[10,2] <- p[4]*p[8]
+  R[12,12] <- p[11]+(1-p[6])*p[12]
+  R[15,12] <- (1-p[6])*p[12]
+  R[15,15] <- p[12]
+  R[6,6] <- p[13]
+  
+  
+  W(stage2NRDLM) <- R%*%t(R)
+  
+  # PRIORS
+  m0(stage2NRDLM) <- c(NRdata$log.output[1],0,0,0,mean(diff(NRdata$log.output[1:4])),NRdata$real.r[2],NRdata$real.r[1],0,NRdata$unr[1],0,0,NRdata$Inflation[3],NRdata$Inflation[2],NRdata$Inflation[1], NRdata$Inflation.e[1])
+  
+  
+  
+  return(stage2NRDLM)
+  
+}
+
+theta <- c(1.53,-0.54 ,-0.15, -0.62, -0.32, 0.5, sqrt(0.38), sqrt(0.54), sqrt(0.15), sqrt(0.07),sqrt(0.79),0.5,0.5, 0) # estimates from RBA  paper
+#theta <- c(1.53,-0.54 ,-0.15, -0.62, -0.32, 0.5, 0.5, 0.5, 0.5, 0.5,0.5,0.5) #
+
+LC <- rep(-Inf,14)
+UC <- rep(Inf,14)
+UC[3] <- -0.0025
+UC[5] <- -0.25   # Parameter on Phillips curve is problematic
+LC[c(7:12)] <- exp(-8)
+UC[c(7:12)] <- exp(12)
+
+
+stage2NRDLM.est <-  dlmMLE(y = cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation, NRdata$Inflation.e, NRdata$real.r), parm = theta, build = stage2buildNRDLM, 
+                           lower = LC, upper = UC,
+                           control = list(trace = 6, REPORT = 5, maxit = 1000), debug = FALSE, method = "L-BFGS-B", hessian = TRUE)
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+# filtered and smoothed estimates
+#--------------------------------------------------------------------------------------------------------------------------
+
+stage2NRDLMbuilt <- stage2buildNRDLM(stage2NRDLM.est$par)
+
+
+stage2filtered <- dlmFilter(y =cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation, NRdata$Inflation.e,NRdata$real.r), mod = stage2NRDLMbuilt)
+
+stage2smoothed <- dlmSmooth(y = cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation,NRdata$Inflation.e,NRdata$real.r), mod = stage2NRDLMbuilt)
+
+
+cbind(stage2filtered$y[-c(2:9),1],stage2filtered$m[-c(1:9),1]) %>% matplot(type ="l")
+
+cbind(stage2filtered$y[-c(2:9),1],stage2smoothed$s[-c(1:9),1]) %>% matplot(type ="l")
+
+cbind(stage2filtered$m[-c(1:9),5],stage2smoothed$s[-c(1:9),5]) %>% matplot(type ="l")
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+# Median unbiased estimate of the variance for mu (ypot growth) 
+#--------------------------------------------------------------------------------------------------------------------------
+output.gap.smoothed <- stage2smoothed$s[,2] 
+
+## Inputs for median.unbiased.estimator.stage2.R
+y <- output.gap.smoothed[3:length(output.gap.smoothed)]
+x <- cbind(output.gap.smoothed[2:(length(output.gap.smoothed)-1)],
+           output.gap.smoothed[1:(length(output.gap.smoothed)-2)],
+           (NRdata$real.r[2:length(NRdata$real.r)]+NRdata$real.r.l1[2:length(NRdata$real.r)])/2,
+           stage2smoothed$s[2:(length(output.gap.smoothed)-1),5],
+           rep(1,length(NRdata$log.output)-1))
+
+lambda.z <- median.unbiased.estimator.stage2(y,x)
+
+#--------------------------------------------------------------------------------------------------------------------------
+# Stage 3 
+#---------------------------------------------------------------------------------------------------------------
+
+stage3NRDLM <- dlm(
+  
+  FF = matrix(0,5,17),
+  
+  V = diag(0.00001, 5),
+  
+  GG =diag(0,17),
+  
+  W = diag(0,17),
+  
+  m0 = rep(0,17),
+  
+  C0 = diag(10000000,17)
+  
+  
+)
+
+# Matrix to parametrise VCV matrix W
+R <- diag(0,17)
+
+
+# build DLM
+stage3buildNRDLM <- function(p){
+  
+  FF(stage3NRDLM)[1,1] <- 1
+  FF(stage3NRDLM)[1,2] <- 1
+  
+  FF(stage3NRDLM)[2,11] <- 1
+  FF(stage3NRDLM)[2,12] <- 1
+  
+  FF(stage3NRDLM)[3,14] <- 1
+  
+  FF(stage3NRDLM)[4,17] <- 1
+  
+  FF(stage3NRDLM)[5,7] <- 1
+  
+  
+  GG(stage3NRDLM)[1,1]  <- 1 
+  GG(stage3NRDLM)[1,5]  <- 1
+  GG(stage3NRDLM)[2,2]  <- p[1] 
+  GG(stage3NRDLM)[2,3]  <- p[2]
+
+  GG(stage3NRDLM)[2,7]  <- p[3]/2
+  GG(stage3NRDLM)[2,8]  <- p[3]/2
+  GG(stage3NRDLM)[2,5]  <- -p[4]*p[3]/2
+  GG(stage3NRDLM)[2,6]  <- -p[4]*p[3]/2
+  
+  GG(stage3NRDLM)[2,9]  <- -p[3]/2
+  GG(stage3NRDLM)[2,10]  <- -p[3]/2
+  
+  
+  GG(stage3NRDLM)[3,2]  <- 1 #ygap(-1)
+  GG(stage3NRDLM)[4,3]  <- 1
+  
+  GG(stage3NRDLM)[5,5] <- 1  # mu
+  GG(stage3NRDLM)[6,5] <- 1
+  
+  GG(stage3NRDLM)[7,7] <- 1   # r
+  GG(stage3NRDLM)[8,7] <- 1
+  
+  GG(stage3NRDLM)[9,9] <- 1  # z
+  GG(stage3NRDLM)[10,9] <- 1
+  
+  GG(stage3NRDLM)[11,11] <- 1  #u*
+  
+  GG(stage3NRDLM)[12,2]  <- p[5]*(0.4*p[1]+0.3)        
+  GG(stage3NRDLM)[12,3]  <- p[5]*(0.4*p[2]+0.2)        
+  GG(stage3NRDLM)[12,4]  <- p[5]*0.1       
+  GG(stage3NRDLM)[12,5]  <- -p[5]*0.4*p[4]*p[3]/2       
+  GG(stage3NRDLM)[12,6]  <- -p[5]*0.4*p[4]*p[3]/2       
+  GG(stage3NRDLM)[12,7] <- p[5]*0.4*p[3]/2            
+  GG(stage3NRDLM)[12,8] <- p[5]*0.4*p[3]/2  
+  GG(stage3NRDLM)[12,9] <- -p[5]*0.4*p[3]/2            
+  GG(stage3NRDLM)[12,10] <- -p[5]*0.4*p[3]/2  
+  
+  
+  GG(stage3NRDLM)[13,12] <- 1           
+  
+  GG(stage3NRDLM)[14,13] <- p[6]
+  GG(stage3NRDLM)[14,14] <- p[7]/3
+  GG(stage3NRDLM)[14,15] <- p[7]/3
+  GG(stage3NRDLM)[14,16] <- p[7]/3
+  GG(stage3NRDLM)[14,17] <- (1-p[7])
+  GG(stage3NRDLM)[15,14] <- 1
+  GG(stage3NRDLM)[16,15] <- 1
+  
+  GG(stage3NRDLM)[17,17] <- 1
+  
+  
+  # Variance covariance - RR'
+  
+  # R[1,1] <- (1+lambda.g)*p[8]
+  # R[5,1] <- lambda.g*p[8]
+  # R[2,2] <- p[9]
+  # R[5,5] <-lambda.g*p[8]
+  # R[9,9] <- (lambda.z*p[9])/p[3]
+  # R[11,11] <- p[10]
+  # R[12,12] <- p[11]+p[5]*p[9]
+  # R[12,2] <- p[5]*p[9]
+  # R[14,14] <- p[12]+(1-p[7])*p[13]
+  # R[17,14] <- (1-p[7])*p[13]
+  # R[17,17] <- p[13]
+  # R[7,7] <- p[14]
+  
+
+   R[1,1] <- (1+lambda.g^2)*p[8]^2
+   R[5,1] <- R[1,5] <- (lambda.g*p[8])^2
+   R[2,2] <- p[9]^2
+   R[5,5] <- (lambda.g*p[8])^2
+   R[9,9] <- (lambda.z*p[9]/p[3])^2
+   R[11,11] <- p[10]^2
+   R[12,12] <- p[11]^2+(p[5]*p[9])^2
+   R[12,2] <-  R[2,12] <-  (p[5]*p[9])^2
+   R[14,14] <- p[12]^2+((1-p[7])*p[13])^2
+   R[17,14] <- R[14,17] <-  ((1-p[7])*p[13])^2
+   R[17,17] <- p[13]^2
+   R[7,7] <- p[14]^2
+  
+    
+  W(stage3NRDLM) <- R #R%*%t(R)
+  
+  # PRIORS
+  m0(stage3NRDLM) <- c(NRdata$log.output[1],0,0,0,mean(diff(NRdata$log.output[1:4])),mean(diff(NRdata$log.output[1:5])),NRdata$real.r[2],NRdata$real.r[1],0,0,NRdata$unr[1],0,0,NRdata$Inflation[3],NRdata$Inflation[2],NRdata$Inflation[1], NRdata$Inflation.e[1])
+  
+  
+  
+  return(stage3NRDLM)
+  
+}
+
+theta <- c(1.53,-0.54 ,-0.15, 1, -0.62, -0.32, 0.5, sqrt(0.38), sqrt(0.54), sqrt(0.15), sqrt(0.07),sqrt(0.79),0.5,0.5) # estimates from RBA  paper
+
+LC <- rep(-Inf,14)
+UC <- rep(Inf,14)
+UC[3] <- -0.005  # Calibrated to give a neutral rate consistent with other studies 
+UC[6] <- -0.32   # Parameter on Phillips curve is problematic
+LC[c(7:14)] <- exp(-8)
+UC[c(7:14)] <- exp(12)
+
+
+stage3NRDLM.est <-  dlmMLE(y = cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation, NRdata$Inflation.e, NRdata$real.r), parm = theta, build = stage3buildNRDLM, 
+                           lower = LC, upper = UC,
+                           control = list(trace = 6, REPORT = 5, maxit = 1000), debug = FALSE, method = "L-BFGS-B", hessian = TRUE)
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+# filtered and smoothed estimates
+#--------------------------------------------------------------------------------------------------------------------------
+
+# Model is estimating a postive output gap / rnu gap. Most likely something going on with phillips curve. 
+
+stage3NRDLMbuilt <- stage3buildNRDLM(stage3NRDLM.est$par)
+
+stage3filtered <- dlmFilter(y =cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation, NRdata$Inflation.e,NRdata$real.r), mod = stage3NRDLMbuilt)
+
+stage3smoothed <- dlmSmooth(y = cbind(NRdata$log.output,NRdata$unr,NRdata$Inflation,NRdata$Inflation.e,NRdata$real.r), mod = stage3NRDLMbuilt)
+
+
+## One-sided (filtered) R-star estimates
+trend.filtered      <- stage3filtered$m[,4] * 4
+z.filtered          <- stage3filtered$m[,9]
+rstar.filtered      <- trend.filtered * stage3NRDLM.est$par[4] + z.filtered
+
+cbind(NRdata$real.r,rstar.filtered[-1])
+
+## One-sided (smoothed) R-star estimates
+trend.smoothed     <- stage3smoothed$s[,5] * 4
+z.smoothed          <- stage3smoothed$s[,9]
+rstar.smoothed      <- trend.smoothed * stage3NRDLM.est$par[4] + z.smoothed
+
+cbind(NRdata$real.r[-c(1:9)],rstar.smoothed[-c(1:10)]) %>% 
+  matplot(type = "l")
+
+
+cbind(stage3filtered$y[-c(2:9),1],stage3filtered$m[-c(1:9),1]) %>% matplot(type ="l")
+
+cbind(stage3filtered$y[-c(2:9),1],stage3smoothed$s[-c(1:9),1]) %>% matplot(type ="l")
+
+cbind(stage3filtered$m[-c(1:9),5],stage3smoothed$s[-c(1:9),5]) %>% matplot(type ="l")
+
+#--------------------------------------------------------------------------------------------------------------------------
+# Charts and tables
+#--------------------------------------------------------------------------------------------------------------------------
+
+# Chart one - potential output and actual output
+
+# Chart two - output gap
+
+# Chart three - nairu and unemployment rate
+
+# Chart four - r* and real cash rate
 
 
 
